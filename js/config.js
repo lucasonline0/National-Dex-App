ddbc673928ae374a449723cab3c42889abbe2bb8
Lucas <129661036+lucasonline0@users.noreply.github.com>
2025-09-20 17:21:23 -0300

    refactor(js): Extrai variáveis de configuração para o módulo config.js

---
+++ js/config.js
@@ -0,0 +1,19 @@
+const typeColors = {
+    normal: 'bg-gray-400', fire: 'bg-red-500', water: 'bg-blue-500',
+    electric: 'bg-yellow-400', grass: 'bg-green-500', ice: 'bg-cyan-300',
+    fighting: 'bg-orange-700', poison: 'bg-purple-600', ground: 'bg-yellow-600',
+    flying: 'bg-indigo-400', psychic: 'bg-pink-500', bug: 'bg-lime-500',
+    rock: 'bg-yellow-700', ghost: 'bg-indigo-800', dragon: 'bg-indigo-600',
+    dark: 'bg-gray-700', steel: 'bg-gray-500', fairy: 'bg-pink-300'
+};
+
+const toolTitles = {
+    'team-builder': 'Construtor de Times',
+    'comparator': 'Comparador de Pokémon',
+    'game': 'Quem é este Pokémon?',
+    'trainer-card': 'Cartão de Treinador',
+    'random-team': 'Gerador de Time Aleatório',
+    'type-coverage': 'Análise de Cobertura de Tipos',
+    'abilities': 'Explorador de Habilidades',
+    'timeline': 'Linha do Tempo dos Jogos'
+};
